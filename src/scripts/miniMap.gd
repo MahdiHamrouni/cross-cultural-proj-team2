003a67f196bbc55097ee1e033144ff05d607c175
Gianni <giannitoapanta174@gmail.com>
2026-04-24 14:00:23 +0200

    aggiunta il canvas layer come scena

--- src/scripts/miniMap.gd
+++ src/scripts/miniMap.gd
@@ -12,3 +12,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$Camera2D.position = player.position
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
